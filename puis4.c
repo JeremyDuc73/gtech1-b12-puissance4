a42589c38933086c4620196e201abc6d6c43b512
aleksi bieslki <abielski@gaming.tech>
2021-10-20 19:08:08 +0200

    puissance_final

--- puis4.c
+++ puis4.c
@@ -26,6 +26,10 @@ void display(void) {
   printf("\n");
   tiret();
   printf("\n");
+  for(int r =1; r<NBC+1;r++){
+    printf("%d ",r);
+  }
+  printf("\n");
 }
 
 void init(void){
@@ -61,6 +65,57 @@ int pion_in_tab(int col_pion, int player){
   }
 }
 
+int win=0;
+void win_vertical(void){
+  for(int r=0;r<NBR-3;r++) {
+    for(int c=0;c<NBC;c++) {
+      if (gamearray[r][c]==token[player] && gamearray[r+1][c]==token[player] && gamearray[r+2][c]==token[player] && gamearray[r+3][c]==token[player]){
+	win=1;
+      }
+    }
+  }
+}
+
+
+void win_horizontal(void){
+  for(int r=0;r<NBR;r++) {
+    for(int c=0;c<NBC-3;c++) {
+      if (gamearray[r][c]==token[player] && gamearray[r][c+1]==token[player] && gamearray[r][c+2]==token[player] && gamearray[r][c+3]==token[player]){
+	win=1;
+      }
+    }
+  }
+}
+
+
+void win_diagonal_left(void){
+  for(int r=0;r<NBR-3;r++) {
+    for(int c=0;c<NBC-3;c++) {
+      if (gamearray[r][c]==token[player] && gamearray[r+1][c+1]==token[player] && gamearray[r+2][c+2]==token[player] && gamearray[r+3][c+3]==token[player]){
+        win = 1;
+      }
+    }
+  }
+}
+
+void win_diagonal(void){
+  for(int r=0;r<NBR-3;r++) {
+    for(int c=NBC; c>2 ;c--) {
+      if (gamearray[r][c]==token[player] && gamearray[r+1][c-1]==token[player] && gamearray[r+2][c-2]==token[player] && gamearray[r+3][c-3]==token[player]){
+	win = 1;
+      }
+    }
+  }
+}
+
+void verify_win(void){
+  win_vertical();
+  win_horizontal();
+  win_diagonal_left();
+  win_diagonal();
+}
+
+
 int main (void){
   init();
   while (end_game ==0) {
@@ -72,18 +127,23 @@ int main (void){
       pion_in_tab(rep_c, player);
     }
     display();
-    if (player == 0){
-      player = 1;
+    verify_win();
+    
+    if (win == 1){
+      printf("[player %d] : You win\n",player+1);
+      end_game=1 ;
     }
-    else {
-      player = 0;
-    }
-    tour = 0;
-    end += 1;
-    if (end == 42){
+    
+    else if (end == 42){
       printf("Sorry but it's a draw this time !\n");
       end_game = 1;
     }
+
+
+    player= !player;
+    tour = 0;
+    end += 1;
+    
   }
 }
 
